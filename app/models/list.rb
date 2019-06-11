6b654e968fc76af16a0ac9916d7b73d90969af6b
Josh Ellison <joshellison@Jeremiahs-Mac-mini.local>
2019-06-10 19:07:06 -0700

    syncing master for deploy

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,11 +3,11 @@ class List
  #                      SET UP
  # ==================================================
  # add attribute readers for instance accesss
-  attr_reader :id, :list, :completed
+  attr_reader :id, :title, :iscomplete
 
 
-    if(ENV['https://buckidea-api.herokuapp.com/'])
-        uri = URI.parse(ENV['https://buckidea-api.herokuapp.com/'])
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
     else
         DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'plan_it_app_api_development'})
