904f7f1e8979d3df0abb8f8cd6eaa7ed7f362323
Kyungsub Kim <raboriel@gmail.com>
2019-06-12 00:28:46 -0400

    replace imageUrl to image

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,21 +3,21 @@ class List
  #                      SET UP
  # ==================================================
  # add attribute readers for instance accesss
-  attr_reader :id, :title, :imageurl, :description, :done, :likes
+  attr_reader :id, :title, :image, :description, :done, :likes
   # CREATE TABLE lists (id SERIAL, title VARCHAR(50), description VARCHAR(255), done INT, likes INT);
 
     if(ENV['DATABASE_URL'])
         uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
     else
-        DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'plan_it_app_api_development'})
+        DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'bucketapi'})
     end
 # change db name back to bucket-lister-api
     #initialize options Hash
     def initialize(opts = {}, id = nil)
       @id = id.to_i
       @title = opts["title"]
-      @imageurl = opts["imageurl"]
+      @image = opts["image"]
       @description = opts["description"]
       @done = opts["done"].to_i
       @likes = opts["likes"].to_i
@@ -39,9 +39,9 @@ class List
   # create task
   DB.prepare("create_list",
     <<-SQL
-      INSERT INTO lists (title, description, imageurl, likes)
+      INSERT INTO lists (title, description, image, likes)
       VALUES ( $1, $2, $3, $4 )
-      RETURNING id, title, description, imageurl, likes, done;
+      RETURNING id, title, description, image, likes, done;
     SQL
   )
 
@@ -58,9 +58,9 @@ class List
   DB.prepare("update_list",
     <<-SQL
       UPDATE lists
-      SET title = $2, description = $3, imageurl = $4
+      SET title = $2, description = $3, image = $4
       WHERE id = $1
-      RETURNING id, title, description, imageurl;
+      RETURNING id, title, description, image;
     SQL
   )
 
@@ -94,17 +94,18 @@ class List
   # create one
   def self.create(opts)
     # if opts["completed"] does not exist, default it to false
-  p opts
-  p '==================================='
+  # p opts
+  # p '==================================='
     # create the task
-    results = DB.exec_prepared("create_list", [opts["title"], opts["description"], opts["imageURL"], opts["likes"]])
+    results = DB.exec_prepared("create_list", [opts["title"], opts["description"], opts["image"], opts["likes"]])
     # turn completed value into boolean
 
     # return the task
     list = List.new(
       {
         "title" => results.first["title"],
-        # "done" => iscomplete
+        "description" => results.first["description"],
+        "image" => results.first["image"]
       },
       results.first["id"]
     )
@@ -123,7 +124,7 @@ class List
   # update one
   def self.update(id, opts)
     # update the list
-    results = DB.exec_prepared("update_list", [id, opts["title"], opts["description"], opts["imageurl"], opts["likes"]])
+    results = DB.exec_prepared("update_list", [id, opts["title"], opts["description"], opts["image"], opts["likes"]])
     # if results.first exists, it was successfully updated so return the updated list
 
       # return the task
@@ -131,7 +132,7 @@ class List
         {
           "title" => results.first["title"],
           "description" => results.first["description"],
-          "imageurl" => results.first["imageurl"],
+          "image" => results.first["image"],
           "likes" => results.first["likes"],
         },
         results.first["id"]
